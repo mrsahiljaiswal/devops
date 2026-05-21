49d72e566505de8af8c95935b3807ffe5a80cbad
mrsahiljaiswal <sahiljaiswal757@gmail.com>
2026-05-21 11:06:23 +0530

    updated the api endpoints

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/*', cors(corsOptions));
 app.use(express.json());
 app.use(express.static('public'));
 
